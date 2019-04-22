f41ed48388db64f35f5c463587ffeba5df37f6f6
Andrey Chernov <andrii.chernov@gmail.com>
2019-04-22 17:50:06 +0300

    used trim in parsing product name

--- src/main/java/ua/eprice/services/vseceni/ProductVseceniService.java
+++ src/main/java/ua/eprice/services/vseceni/ProductVseceniService.java
@@ -40,7 +40,7 @@ public class ProductVseceniService {
     private String getName(Document doc) {
         String name = doc.select("h1 span").html();
         if (name.indexOf("<small>") >= 0) name = name.substring(0, name.indexOf("<small>"));
-        return name;
+        return name.trim();
     }
 
     private Integer getQuantityOffers(Document doc) {
